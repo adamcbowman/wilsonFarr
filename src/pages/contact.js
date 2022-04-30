e7a464ae20eec30ffc99ce01e3c641f56e389848
Adam Bowman <adamcbowman@gmail.com>
2022-04-29 23:20:02 -0300

    add google contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import Layout from '../components/layout/Layout';
+import React from "react";
+import Layout from "../components/layout/Layout";
 
 const contact = () => {
   return (
     <Layout>
       <section>
-        <div className="px-8 items-center text-center bg-white w-full h-screen">
-          <div className="pt-20">
+        <div className="items-center text-center bg-white w-full h-screen">
+          {/* <div className="pt-20">
             <h1 className="text-xl text-black font-bold">Contact Us</h1>
-          </div>
+          </div> */}
+          <object
+            type="text/html"
+            // data="https://adamcbowman.getform.com/gozlx"
+            data="https://docs.google.com/forms/d/e/1FAIpQLSeXlXZm0ueBBhrsSOUl46HSTbAMn7gmFNd2VuQZ6A9PSULoxA/viewform?embedded=true"
+            style={{ width: "100%", height: "100%" }}
+          >
+            {" "}
+          </object>
         </div>
       </section>
     </Layout>
