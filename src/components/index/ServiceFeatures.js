07542ce75769d93184a65da6b4e1d61b889296d9
Adam Bowman <adamcbowman@gmail.com>
2022-04-30 17:05:15 -0300

    Update ServiceFeatures.js
    reorder list for balance

--- src/components/index/ServiceFeatures.js
+++ src/components/index/ServiceFeatures.js
@@ -45,6 +45,7 @@ const ServiceFeatures = () => {
                 <ListItem item="Locally Owned and Operated" />
                 <ListItem item="Staff Background Checks" />
                 <ListItem item="Office Cleaning Service" />
+                <ListItem item="Deep Cleaning Service" />
               </Checklist>
             </div>
             <div className="w-full md:w-1/2">
@@ -52,7 +53,7 @@ const ServiceFeatures = () => {
                 <ListItem item="Professional & Devoted Teams" />
                 <ListItem item="Detail-Oriented" />
                 <ListItem item="Sanitizing Services" />
-                <ListItem item="Deep Cleaning Service" />
+                
                 <ListItem item="Interior & Exterior Window Cleaning" />
                 <ListItem item="Special Requests" />
                 <ListItem item="Eco-Friendly & Green Products" />
