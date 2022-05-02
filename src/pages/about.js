ae32c60c5c415445e87e9aad933937aeb3f05c8d
Adam Bowman <adamcbowman@gmail.com>
2022-05-02 16:00:14 -0300

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -11,7 +11,18 @@ const about = () => {
           style={{ position: `relative` }}
         >
           <div
-            className="w-screen bg-cover"
+            className="w-screen bg-cover hidden md:block"
+            style={{
+              backgroundImage: `url(${bgImg})`,
+              minHeight: `400px`,
+              minWidth: `100%`,
+              backgroundSize: `cover`,
+              backgroundPosition: `center`,
+              opacity: `65%`,
+            }}
+          ></div>
+          <div
+            className="w-screen bg-cover block md:hidden "
             style={{
               backgroundImage: `url(${bgImg})`,
               minHeight: `300px`,
@@ -25,13 +36,13 @@ const about = () => {
             className="w-full justify-items-center align-items-center text-center"
             style={{ position: `absolute`, width: `100vw` }}
           >
-            <h1 className="text-4xl text-white font-bold">About Us</h1>
+            <h1 className="text-4xl text-white font-bold">About</h1>
           </div>
         </div>
       </section>
       <section id="about">
         <div className="flex flex-col justify-center items-center bg-white p-8 md:p-20 text-justify ">
-          <p className="text-gray-800 font-light p-2">
+          <p className="text-gray-800 font-light p-2 text-justify">
           Wilson & Farr Cleaning Solutions was established in 2020 by
           proprietors Alex and Tamara. Using their managerial skills,
           backgrounds in production facilities and healthcare they were able to
@@ -39,15 +50,15 @@ const about = () => {
           operations. Quality, integrity and professionalism were integral
           values that enabled them to thrive in their fields. 
           </p>
-          <p className="text-gray-800 font-light p-2">
+          <p className="text-gray-800 font-light p-2 text-justify">
           Helping their clients celebrate milestones and assisting clients
           through life transitions, Wilson & Farr Cleaning Solutions helps to
           give back to their community. Whether preparing homes for listing,
           assisting older adults, alleviating stress in busy households, or
           creating a safe and healthy work environment, they are devoted to
-          making spaces sparkle. 
+          making spaces sparkle.
           </p>
-          <p className="text-gray-800 font-light p-2">
+          <p className="text-gray-800 font-light p-2 text-justify">
           Their goal when starting Wilson & Farr Cleaning Solutions was simple;
           to exceed client’s expectations by providing outstanding customer
           service, and delivering meticulous quality-focused cleaning
